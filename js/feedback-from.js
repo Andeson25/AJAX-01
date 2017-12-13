7a395129754c64c5e4d7049f92a881ccaa42ecc3
kek <oleh.petryk@gmail.com>
2017-12-13 18:40:44 +0200

    c

--- js/feedback-from.js
+++ js/feedback-from.js
@@ -1,19 +1,20 @@
-$(function() {
-  $("#contact-form input[type=submit]").click(sendForm);
-});
-
-function sendForm(ev) {
-  ev.preventDefault();
-  $.ajax({
-    url: "https://formspree.io/oleh.petryk@gmail.com",
-    method: "POST",
-    data: {
-      name: $("#name").val(),
-      email: $("#email").val(),
-      feedback: $("#feedback").val()
-    },
-    dataType: "json"
-  })
-    .done(() => $("#thank-dialog").attr("open", "open"))
-    .fail(() => $("#error-dialog").attr("open", "open"));
-}
+$(function(){
+    $('#contact-form input[type=submit]').click(sendForm); 
+ });
+ 
+ function sendForm(ev) {
+     
+     ev.preventDefault();
+     $.ajax({
+         url: "https://formspree.io/oleh.petryk@gmail.com", 
+         method: "POST",
+         data: {
+             name: $('#name').val(),
+             email: $('#email').val(),
+             feedback: $('#feedback').val()
+         },
+         dataType: "json"
+     })
+      .done( () => $('#thank-dialog').attr('open', 'open') )
+      .fail( () => $('#error-dialog').attr('open', 'open') );
+ }
