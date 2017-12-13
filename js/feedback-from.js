4bf3f7d8f35ed4c1ec1ca7364ee156c2328cbf46
kek <oleh.petryk@gmail.com>
2017-12-13 18:36:51 +0200

    c

--- js/feedback-from.js
+++ js/feedback-from.js
@@ -14,5 +14,6 @@ function sendForm(ev) {
     },
     dataType: "json"
   })
-  .done(() => $("#thank-dialog").attr("open", "open"));
+    .done(() => $("#thank-dialog").attr("open", "open"))
+    .fail(() => $("#error-dialog").attr("open", "open"));
 }
