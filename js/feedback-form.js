299af3276e79064b835f35526c4e334b02c38845
kek <oleh.petryk@gmail.com>
2017-12-16 13:51:44 +0200

    c

--- js/feedback-form.js
+++ js/feedback-form.js
@@ -1,30 +1,32 @@
-$(document).ready(function() {
-  $("#ajaxform").submit(function() {
-    var form = $(this);
-    var error = false; 
-    form.find("input, textarea").each(function() {
-      if ($(this).val() == "") {
-        alert('Заповніть пoлe "' + $(this).attr("placeholder") + '"!'); 
-        error = true; 
-      }
+$(document).ready(function () {
+    $("#ajaxform").submit(function () {
+        var form = $(this);
+        var error = false;
+        form.find("input, textarea").each(function () {
+            if ($(this).val() == "") {
+                alert('Заповніть пoлe "' + $(this).attr("placeholder") + '"!');
+                error = true;
+            }
+        });
+        if (!error) {
+            var data = form.serialize();
+            $.ajax({
+                type: "POST",
+                url: "https://formspree.io/oleh.petryk@gmail.com",
+                dataType: "json",
+                data: data,
+                success: () => {
+                alert("Форма відправлена успішно!");
+        },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert(xhr.status);
+                alert(thrownError);
+                alert("Помилка!")
+            }
+        ,
+        })
+            ;
+        }
+        return false;
     });
-    if (!error) {
-      var data = form.serialize(); 
-      $.ajax({
-        type: "POST", 
-        url: "https://formspree.io/oleh.petryk@gmail.com", 
-        dataType: "json", 
-        data: data,
-        success: ()=> {
-            alert("Форма відправлена успішно!"); 
-        },
-        error: function(xhr, ajaxOptions, thrownError) {
-          alert(xhr.status); 
-          alert(thrownError); 
-          alert("Помилка!")
-        },
-      });
-    }
-    return false; 
-  });
 });
